c08da054ce28ed15e0495f48272c7158d6ad0972
jberthiaume1138 <joenyc23@gmail.com>
2017-01-17 03:12:35 -0500

    more basic styling

--- public/src/js/pages/About.js
+++ public/src/js/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import style from './style/About.scss';
+
 export default class About extends React.Component {
 	render() {
 		return (
 			<div>
 				<p>About me...</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p className={style.p}>When in the Course of human events it becomes necessary for one people to dissolve the political bands which have connected them with another and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.</p>
 			</div>
 		);
 	}
--- public/src/js/pages/Home.js
+++ public/src/js/pages/Home.js
@@ -5,7 +5,6 @@ export default class Home extends React.Component {
 		return (
 			<main>
 				<p>Home Content</p>
-				<p>When in the Course of human events it becomes necessary for one people to dissolve the political bands which have connected them with another and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.</p>
 			</main>
 		);
 	}
