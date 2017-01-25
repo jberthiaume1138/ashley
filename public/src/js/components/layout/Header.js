2558f282d0ac48bf4111aefe0e0aea5255e8db45
jberthiaume1138 <joenyc23@gmail.com>
2017-01-24 23:16:23 -0500

    styling general

--- public/src/js/components/layout/Header.js
+++ public/src/js/components/layout/Header.js
@@ -8,7 +8,7 @@ export default class Header extends React.Component {
 		return (
 			<header className={styles.banner}>
 				<Link to="/"><h1 className={styles.bannerH1}>Your Name</h1></Link>
-				<h4 className={styles.bannerH2}>Some Sub-Header Blurb</h4>
+				<h4 className={styles.bannerH2}>Some Sub-Header Blurb9999</h4>
 			</header>
 		);
 	}
