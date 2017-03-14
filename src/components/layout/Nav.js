2f89e3211d8e7e8590eed53fb5caf3aeb1c7dbe0
jberthiaume1138 <joenyc23@gmail.com>
2017-03-14 02:19:45 -0400

    fix header / about styling

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -6,10 +6,12 @@ import style from './style/Nav.scss';
 export default class Nav extends React.Component {
 	render() {
 		return (
-			<nav className={style.navBar}>
-				<Link className={style.navLink} to="about">About</Link>
-				<Link className={style.navLink} to="portfolio">Portfolio</Link>
-				<Link className={style.navLink} to="contact">Contact</Link>
+			<nav>
+				<div className={style.wrapper}>
+					<Link className={style.navLink} to="about">About</Link>
+					<Link className={style.navLink} to="portfolio">Portfolio</Link>
+					<Link className={style.navLink} to="contact">Contact</Link>
+				</div>
 			</nav>
 		);
 	}
