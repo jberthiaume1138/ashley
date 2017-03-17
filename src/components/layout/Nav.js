4242a762d66d234bb18d99be5dbc9f43511aeeb6
jberthiaume1138 <joenyc23@gmail.com>
2017-03-16 21:50:41 -0400

    Nav active color, adjust base color

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -8,10 +8,10 @@ export default class Nav extends React.Component {
 		return (
 			// <nav>
 				<div className={styles.wrapper}>
-					<Link className={styles.navLink} to='about'>About</Link>
-					<Link className={styles.navLink} to='portfolio'>Portfolio</Link>
-					<Link className={styles.navLink} to='press'>Press</Link>
-					<Link className={styles.navLink} to='contact'>Contact</Link>
+					<Link className={styles.navLink} activeClassName={styles.navActive} to='about'>About</Link>
+					<Link className={styles.navLink} activeClassName={styles.navActive} to='portfolio'>Portfolio</Link>
+					<Link className={styles.navLink} activeClassName={styles.navActive} to='press'>Press</Link>
+					<Link className={styles.navLink} activeClassName={styles.navActive} to='contact'>Contact</Link>
 				</div>
 			// </nav>
 		);
