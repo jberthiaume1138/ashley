8ca0291d5046481ec8211b75d2b09595687311eb
jberthiaume1138 <joenyc23@gmail.com>
2017-03-18 02:25:10 -0400

    adding padding to navLinks to increase usability

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -6,14 +6,12 @@ import styles from './style/Nav.scss';
 export default class Nav extends React.Component {
 	render() {
 		return (
-			// <nav>
-				<div className={styles.wrapper}>
-					<Link className={styles.navLink} activeClassName={styles.navActive} to='about'>About</Link>
-					<Link className={styles.navLink} activeClassName={styles.navActive} to='portfolio'>Portfolio</Link>
-					<Link className={styles.navLink} activeClassName={styles.navActive} to='press'>Press</Link>
-					<Link className={styles.navLink} activeClassName={styles.navActive} to='contact'>Contact</Link>
-				</div>
-			// </nav>
+			<div className={styles.wrapper}>
+				<Link className={styles.navLink} activeClassName={styles.navActive} to='about'>About</Link>
+				<Link className={styles.navLink} activeClassName={styles.navActive} to='portfolio'>Portfolio</Link>
+				<Link className={styles.navLink} activeClassName={styles.navActive} to='press'>Press</Link>
+				<Link className={styles.navLink} activeClassName={styles.navActive} to='contact'>Contact</Link>
+			</div>
 		);
 	}
 }
