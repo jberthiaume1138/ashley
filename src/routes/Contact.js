cbf6cdd5c4f435425cb10c232159754b9895b115
jberthiaume1138 <joenyc23@gmail.com>
2017-03-24 23:04:14 -0400

    add banner test

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import sketch from '../images/comp-sketch-2.jpg';
+import gfr from '../images/gfr-banner.jpg';
 
 import styles from './style/Contact.scss'
 
@@ -14,7 +15,9 @@ export default class Contact extends React.Component {
 				<div className={styles.textWrapper}>
 					<h4 className={styles.headline}>Contact me</h4>
 					<p className={styles.mainText}>LinkedIn - https://www.linkedin.com/in/ashleyberthiaume//</p>
-
+					<div className={styles.imageWrapper}>
+						<img src={gfr}/>
+					</div>
 					<p className={styles.mainText}>Instagram - https://www.instagram.com/gofollowrabbits/</p>
 					<p className={styles.mainText}>https://www.facebook.com/GofollowRabbits/</p>
 					<p className={styles.mainText}>Vist the Go Follow Rabbits shop! https://www.etsy.com/shop/gofollowrabbits/</p>
