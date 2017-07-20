419cec9691436c5cc63272bc03ae3647ae1a8e54
jberthiaume1138 <joenyc23@gmail.com>
2017-07-20 17:41:57 -0400

    update to latest create-react-app

--- src/components/gallery/Pagination.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Pagination extends React.Component {
-	render() {
-		return (
-			<button onClick={this.props.onClick}>{this.props.label}</button>
-		)
-	}
-}
