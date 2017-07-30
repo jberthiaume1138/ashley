77cd93a9bcf0b2e81ec4157a06ca90d681009557
jberthiaume1138 <joenyc23@gmail.com>
2017-07-30 03:13:21 -0400

    Convert header to stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 import styles from './styles/Header.css';
 
-export default class Header extends React.Component {
-	render () {
-		return (
-			<div className={styles.wrapper}>
-				<Link to="/"><h1 className={styles.headline}>Ashley Berthiaume</h1></Link>
-				<h2 className={styles.subheader}>Fashion Designer</h2>
-			</div>
-		);
-	}
+const Header = () => {
+	return (
+		<div className={styles.wrapper}>
+			<Link to="/"><h1 className={styles.headline}>Ashley Berthiaume</h1></Link>
+			<h2 className={styles.subheader}>Fashion Designer</h2>
+		</div>
+	);
 }
+
+export default Header;
